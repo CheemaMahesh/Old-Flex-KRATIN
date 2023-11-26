ce597596d0859718de9612dc50616673e0aab46d
Cheema Mahesh <maheshbabucheema789@gmail.com>
2023-11-26 22:42:26 +0530

    Up

--- src/Componets/Pages/Home.jsx
+++ src/Componets/Pages/Home.jsx
@@ -13,6 +13,15 @@ export default function Home(){
 "Every rep, every lift, every step - a journey towards strength, health, and self-discovery.
                 </div>
             </div>
+
+            <div className={styles.fitness}>
+                <div className={styles.top}>
+                Younger Every Day
+                </div>
+                <div className={styles.bottom}>
+                Regular physical activity is an essential cornerstone of a healthy lifestyle. Exercise isn't merely about lifting weights or running miles; it's a holistic practice that nourishes the mind, body, and spirit. It's a journey of self-discovery, resilience-building, and constant growth
+                </div>
+            </div>
         </div>
     )
 }
